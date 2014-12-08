991a8b424f99f089d23409b1fa499b8dcbe425ce
OwnDroid <askowndroid@gmail.com>
2014-12-08 13:49:59 +0100

    fix root???

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,6 @@
 PRODUCT_BRAND ?= ownrom
 
 SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
 
 ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
 # determine the smaller dimension
@@ -96,9 +95,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/ownrom/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
-# CM-specific init file
+# OwnROM-specific init file
 PRODUCT_COPY_FILES += \
     vendor/ownrom/prebuilt/common/etc/init.local.rc:root/init.ownrom.rc
+    external/koush/Superuser/init.superuser.rc:root/init.superuser.rc
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
@@ -213,10 +213,6 @@ PRODUCT_COPY_FILES +=  \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=3
-else
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.root_access=0
 
 endif
 
